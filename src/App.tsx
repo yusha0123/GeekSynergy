0b20feccfcf001a8e53a79f065a3784b70b8aa76
Yusha Tahlil <ymmttmy5@gmail.com>
2024-04-08 11:59:45 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { lazy } from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const App = () => {
+  const Home = lazy(() => import("./pages/Home"));
+  const Signup = lazy(() => import("./pages/Signup"));
+  const Login = lazy(() => import("./pages/Login"));
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        {/* <Route element={<PublicRoute />}> */}
+        <Route path="/" element={<Home />} />
+        {/* </Route> */}
+        {/* <Route element={<PrivateRoute />}> */}
+        <Route path="login" element={<Login />} />
+        <Route path="sign-up" element={<Signup />} />
+        {/* </Route> */}
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
